6805822d19b3a7dc73d27b15ab8f4802a896e7b8
FreeMasen <RFM@Herman.local>
2018-01-19 07:47:37 -0600

    a little further

--- src/pony_builder.rs
+++ src/pony_builder.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-
+use std::path::{PathBuf};
 // use hyper::server::NewService;
 
 use super::pony::{Pony};
@@ -72,22 +72,72 @@ impl PonyBuilder {
 }
 
 impl PonyBuilder {
-    pub fn get(&mut self, path: String, cb: Callback) -> &Self {
+    pub fn get(&mut self, path: String, cb: Callback) -> &mut PonyBuilder {
+        self.gets.insert(path, cb);
         self
     }
-    pub fn post(&mut self, path:String, cb: Callback) -> &Self {
+    pub fn post(&mut self, path: String, cb: Callback) -> &mut PonyBuilder {
+        self.posts.insert(path, cb);
         self
     }
-    pub fn put(&mut self, path: String, cb: Callback) -> &Self {
+    pub fn put(&mut self, path: String, cb: Callback) -> &mut PonyBuilder {
+        self.puts.insert(path, cb);
         self
     }
-    pub fn delete(&mut self, path: String, cb: Callback) -> &Self {
+    pub fn delete(&mut self, path: String, cb: Callback) -> &mut PonyBuilder {
+        self.deletes.insert(path, cb);
         self
     }
-    pub fn use_static(&mut self, path: String) -> &Self {
+    pub fn use_static(&mut self, path: String) -> &mut PonyBuilder {
+        self.static_path = path;
+        self.static_enabled = true;
         self
     }
-    pub fn use_not_found(&mut self, path: String) -> &Self {
+    pub fn use_not_found(&mut self, path: &str) -> &mut PonyBuilder {
+        let as_buf = PathBuf::from(&path);
+        if !as_buf.exists() {
+            panic!(format!("Not found path does not exist\n{:?}", &path));
+        }
+        self.not_found_path = path.to_string();
+        self.custom_not_found = true;
         self
+    }
+}
+
+
+#[cfg(test)]
+#[allow(unused_variables, dead_code)]
+mod tests {
+    use futures::future::ok;
+    use hyper::{Response, Request};
+    fn res(_req: Request) -> super::super::HyperResult {
+        Box::new(
+            ok(
+                Response::new()
+            )
+        )
+    }
+    #[test]
+    #[should_panic]
+    fn not_found_failed() {
+        let mut pb = super::PonyBuilder::new();
+        pb.use_not_found("/404.html");
+    }
+    #[test]
+    fn not_found() {
+        let mut pb = super::PonyBuilder::new();
+        pb.use_not_found("/");
+        assert!(pb.custom_not_found, true);
+        assert!(pb.not_found_path == String::from("/"), true);
+    }
+    #[test]
+    fn config_test() {
+        let mut pb = super::PonyBuilder::new();
+        pb.use_static(String::from("/"))
+            .use_not_found("/")
+            .get(String::from("/get"), res)
+            .post(String::from("/post"), res)
+            .put(String::from("/put"), res)
+            .delete(String::from("/delete"), res);
     }
 }

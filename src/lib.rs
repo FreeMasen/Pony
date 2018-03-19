2f88c50e0d6258dd27f88d0159b1caa22e7980ac
Robert Masen <rmasen@USIHQ.net>
2018-03-19 11:05:38 -0500

    clean up type

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
-extern crate hyper;
-extern crate futures;
-
+pub extern crate hyper;
+pub extern crate futures;
+// pub use hyper;
+// pub use futures;
 use futures::future::Future;
 use hyper::server::{Request, Response};
 use hyper::Error;
 
-type HyperResult = Box<Future<Item = Response, Error = Error>>;
+
+
+pub type HyperResult = Box<Future<Item = Response, Error = Error>>;
 pub type Callback = fn(Request) -> HyperResult;
 pub mod pony;
 pub mod pony_builder;
